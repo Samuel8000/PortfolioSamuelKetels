c428dfd9409900fcc6825d0173d10ce080806fbb
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-25 16:21:05 +0200

    uploaded language images, logo for nav, skill iq charts. changes favicon. Started ISkillData with GetAllSkills method and mock data service

---
+++ Portfolio/Portfolio.Data/ISkillData.cs
@@ -0,0 +1,13 @@
+﻿using Portfolio.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Portfolio.Data
+{
+    public interface ISkillData
+    {
+        IEnumerable<Skill> GetAllSkills();
+    }
+}
---
+++ Portfolio/Portfolio.Data/InMemorySkillData.cs
@@ -0,0 +1,23 @@
+﻿using Portfolio.Core;
+using System.Collections.Generic;
+
+namespace Portfolio.Data
+{
+    public class InMemorySkillData : ISkillData
+    {
+        List<Skill> skills;
+
+        public InMemorySkillData()
+        {
+            skills = new List<Skill>()
+            {
+                new Skill {Id = 1, SkillName = "HTML5", SkillDescription="Personal Description and evaluation", LogoFilePath = "html5logo.png", PsChartFilePath="HTMLSkillIQ.png", PsSkillLevel = SkillLevel.Expert, PsSkillIqScore = 230, PsDescription = "Summary of courses followed"},
+                new Skill {Id = 2, SkillName = "CSS3", SkillDescription="Personal Description and evaluation", LogoFilePath = "css3logo.png", PsChartFilePath="CSSSkillIQ.png", PsSkillLevel = SkillLevel.Proficient, PsSkillIqScore = 198, PsDescription = "Summary of courses followed"}
+            };
+        }
+        public IEnumerable<Skill> GetAllSkills()
+        {
+            return skills.OrderBy(s => s.Id);
+        }
+    }
+}
