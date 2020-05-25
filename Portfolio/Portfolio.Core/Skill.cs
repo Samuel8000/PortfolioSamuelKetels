4780943a237c6bb7d5232ae3859d635ddbb7e01c
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-25 14:52:41 +0200

    Properties in Skill class and creation SkillLevel Enum

--- Portfolio/Portfolio.Core/Skill.cs
+++ Portfolio/Portfolio.Core/Skill.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Portfolio.Core
+﻿namespace Portfolio.Core
 {
     public class Skill
     {
+        public int Id { get; set; }
+        public string SkillName { get; set; }
+        public string SkillDescription { get; set; }
+        public string LogoFilePath { get; set; }
+        public string PsDescription { get; set; }
+        public int PsSkillIqScore { get; set; }
+        public string PsChartFilePath { get; set; }
+        public SkillLevel PsSkillLevel { get; set; }
     }
 }
---
+++ Portfolio/Portfolio.Core/SkillLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Core
+{
+    public enum SkillLevel
+    {
+        None,
+        Novice,
+        Proficient,
+        Expert
+    }
+}
