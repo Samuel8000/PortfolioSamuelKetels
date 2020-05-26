7ff6b8cf6221ab9b20e4fcfddc69b2f327d07d5b
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-26 07:46:22 +0200

    created details screen and link to screen

---
+++ Portfolio/Portfolio/Pages/Skills/SkillDetail.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Core;
+
+namespace Portfolio.Pages.Skills
+{
+    public class SkillDetailModel : PageModel
+    {
+        public Skill Skill { get; set; }
+        public void OnGet(int skillId)
+        {
+            Skill = new Skill();
+            Skill.Id = skillId;
+        }
+    }
+}
