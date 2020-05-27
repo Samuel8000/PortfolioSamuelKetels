13df009cf65781aa63f4d810436cb3d3a27505a7
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-27 07:47:14 +0200

    created edit form

--- Portfolio/Portfolio/Pages/CMS/Skills/EditSkill.cshtml.cs
+++ Portfolio/Portfolio/Pages/CMS/Skills/EditSkill.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Portfolio.Core;
 using Portfolio.Data;
 
@@ -12,13 +13,18 @@ namespace Portfolio.Pages.CMS.Skills
     public class EditSkillModel : PageModel
     {
         private readonly ISkillData _skillData;
+        private readonly IHtmlHelper _htmlHelper;
+
         public Skill Skill { get; set; }
-        public EditSkillModel(ISkillData skillData)
+        public IEnumerable<SelectListItem> SkillLevels { get; set; }
+        public EditSkillModel(ISkillData skillData, IHtmlHelper htmlHelper)
         {
             _skillData = skillData;
+            _htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int skillId)
         {
+            SkillLevels = _htmlHelper.GetEnumSelectList<SkillLevel>();
             Skill = _skillData.GetSkillById(skillId);
             if(Skill == null)
             {
