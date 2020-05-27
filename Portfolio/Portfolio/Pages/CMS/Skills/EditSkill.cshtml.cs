f1fc1c8e068fe0ecc4e014288bec7c6ae6321e81
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-27 06:57:14 +0200

    CMS: List and EditPage, added FontAwesome

---
+++ Portfolio/Portfolio/Pages/CMS/Skills/EditSkill.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Core;
+using Portfolio.Data;
+
+namespace Portfolio.Pages.CMS.Skills
+{
+    public class EditSkillModel : PageModel
+    {
+        private readonly ISkillData _skillData;
+        public Skill Skill { get; set; }
+        public EditSkillModel(ISkillData skillData)
+        {
+            _skillData = skillData;
+        }
+        public IActionResult OnGet(int skillId)
+        {
+            Skill = _skillData.GetSkillById(skillId);
+            if(Skill == null)
+            {
+                return RedirectToPage("/Shared/_NotFound");
+            }
+            return Page();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Pages/CMS/Skills/SkillList.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Core;
+using Portfolio.Data;
+
+namespace Portfolio.Pages.CMS.Skills
+{
+    public class SkillListModel : PageModel
+    {
+        private readonly ISkillData _skillData;
+        public IEnumerable<Skill> Skills { get; set; }
+
+        public SkillListModel(ISkillData skillData)
+        {
+            _skillData = skillData;
+        }
+        public void OnGet()
+        {
+            Skills = _skillData.GetAllSkills();
+        }
+    }
+}
