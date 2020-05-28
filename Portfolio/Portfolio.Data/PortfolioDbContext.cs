07aa874d54065da54331dbaddbf5c0cd21ce772c
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-28 19:22:20 +0200

    added seed data for skill

--- Portfolio/Portfolio.Data/PortfolioDbContext.cs
+++ Portfolio/Portfolio.Data/PortfolioDbContext.cs
@@ -13,5 +13,14 @@ namespace Portfolio.Data
 
         }
         public DbSet<Skill> Skills { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>()
+                .HasData(
+                new Skill { Id = 1, SkillName = "HTML5", SkillDescription = "Personal Description and evaluation", LogoFilePath = "html5logo.png", PsChartFilePath = "HTMLSkillIQ.png", PsSkillLevel = SkillLevel.Expert, PsSkillIqScore = 230, PsDescription = "Summary of courses followed" },
+                new Skill { Id = 2, SkillName = "CSS3", SkillDescription = "Personal Description and evaluation", LogoFilePath = "css3logo.png", PsChartFilePath = "CSSSkillIQ.png", PsSkillLevel = SkillLevel.Proficient, PsSkillIqScore = 198, PsDescription = "Summary of courses followed" }
+                );
+        }
     }
 }
