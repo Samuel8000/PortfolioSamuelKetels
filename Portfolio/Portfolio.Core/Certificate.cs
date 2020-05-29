04c87ae1beacdf2dc410a0e7502ce7ee81160ee2
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-29 05:14:35 +0200

    added certificate class, interace , certificate page

---
+++ Portfolio/Portfolio.Core/Certificate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Core
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+        public string CertificateName { get; set; }
+        public int SkillId { get; set; }
+        public Skill Skill { get; set; }
+    }
+}
---
+++ Portfolio/Portfolio.Data/ICertificateData.cs
@@ -0,0 +1,12 @@
+﻿using Portfolio.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Data
+{
+    public interface ICertificateData
+    {
+        IEnumerable<Certificate> GetAllCertificates();
+    }
+}
