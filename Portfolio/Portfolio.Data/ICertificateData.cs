60e17d5903e36b3487818642446c5786f6310626
Samuel Ketels <ketels.samuel@gmail.com>
2020-05-29 06:32:35 +0200

    moved sqlcertdata to new file, cleaned up inmemoryclass

--- Portfolio/Portfolio.Data/ICertificateData.cs
+++ Portfolio/Portfolio.Data/ICertificateData.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Portfolio.Core;
+﻿using Portfolio.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,42 +11,4 @@ namespace Portfolio.Data
         IEnumerable<Certificate> GetAllCertificates();
     }
 
-    public class SqlCertificateData : ICertificateData
-    {
-        private readonly PortfolioDbContext _context;
-
-        public SqlCertificateData(PortfolioDbContext context)
-        {
-            _context = context;
-        }
-        public IEnumerable<Certificate> GetAllCertificates()
-        {
-
-
-            return _context.Certificates
-                  .Include(c => c.Skill)
-                 .OrderBy(c => c.SkillId);
-             
-
-
-        }
-    }
-
-    public class InMemoryCertificateData : ICertificateData
-    {
-        List<Certificate> certificates;
-
-        public InMemoryCertificateData()
-        {
-            certificates = new List<Certificate>()
-            {
-                new Certificate { Id = 1, CertificateName = "HTML5 Fundamentals", SkillId = 1},
-                new Certificate {Id = 2, CertificateName = "Introduction to CSS", SkillId = 2}
-            };
-        }
-        public IEnumerable<Certificate> GetAllCertificates()
-        {
-            return certificates.OrderBy(c => c.SkillId);
-        }
-    }
 }
---
+++ Portfolio/Portfolio.Data/SqlCertificateData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Portfolio.Data
+{
+    public class SqlCertificateData : ICertificateData
+    {
+        private readonly PortfolioDbContext _context;
+
+        public SqlCertificateData(PortfolioDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Certificate> GetAllCertificates()
+        {
+
+
+            return _context.Certificates
+                  .Include(c => c.Skill)
+                 .OrderBy(c => c.SkillId);
+             
+
+
+        }
+    }
+}
