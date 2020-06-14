1dded0f9b22fc1a838c6ffcc2d54901e9498600c
Samuel Ketels <ketels.samuel@gmail.com>
2020-06-14 09:19:39 +0200

    test changes in css and js

--- Portfolio/Portfolio/wwwroot/js/site.js
+++ Portfolio/Portfolio/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(function () {
+    $('input[type=text]').removeClass('form-control');
+    $('input[type=text]').addClass('input-field');
+})
