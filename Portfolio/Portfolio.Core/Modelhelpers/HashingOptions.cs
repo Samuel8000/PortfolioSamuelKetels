18a6da5a3da083812a4a176c921f36c88d073ba3
Samuel Ketels <ketels.samuel@gmail.com>
2020-06-19 22:09:57 +0200

    added paswordhasher

---
+++ Portfolio/Portfolio.Core/Modelhelpers/HashingOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Portfolio.Core.Modelhelpers
+{
+    public sealed class HashingOptions
+    {
+        public int Iterations { get; set; } = 10000;
+    }
+}
---
+++ Portfolio/Portfolio.Core/Modelhelpers/IPasswordHasher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Core.Modelhelpers
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        (bool Verified, bool NeedsUpgrade) Check(string hash, string password);
+    }
+}
