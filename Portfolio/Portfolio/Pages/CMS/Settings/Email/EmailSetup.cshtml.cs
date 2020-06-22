3082ff3fd16d09748adedbe56ea9b0331c713a74
Samuel Ketels <ketels.samuel@gmail.com>
2020-06-22 09:08:09 +0200

    emailsetup

--- Portfolio/Portfolio/Pages/CMS/Settings/Email/EmailSetup.cshtml.cs
+++ Portfolio/Portfolio/Pages/CMS/Settings/Email/EmailSetup.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Core;
 using Portfolio.Data;
 
 namespace Portfolio.Pages.CMS.Settings.Email
@@ -12,13 +13,46 @@ namespace Portfolio.Pages.CMS.Settings.Email
     {
         private readonly IEmailSettingsData _emailSettingsData;
 
+        public EmailSetting EmailSetting { get; set; }
+
         public EmailSetupModel(IEmailSettingsData emailSettingsData)
         {
             _emailSettingsData = emailSettingsData;
         }
-        public void OnGet()
+        public IActionResult OnGet(int? emailSettingsId)
         {
+            if (emailSettingsId.HasValue)
+            {
+                EmailSetting = _emailSettingsData.GetEmailSettingById(emailSettingsId.Value);
+            }
+            else
+            {
+                EmailSetting = new EmailSetting();
+            }
+            if (EmailSetting == null)
+            {
+                return RedirectToPage("/Shared/_NotFound");
+            }
+            return Page();
+        }
 
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(EmailSetting.Id > 0)
+            {
+                _emailSettingsData.UpdateEmailSetting(EmailSetting);
+            }
+            else
+            {
+                _emailSettingsData.NewEmailSetting(EmailSetting);
+            }
+            _emailSettingsData.Commit();
+
+            return RedirectToPage("/Email/EmailAccountsList");
         }
     }
 }
