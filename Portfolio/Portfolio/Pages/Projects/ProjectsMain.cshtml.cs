05c4980f6ad58988bef33513a90e19ef89f74d6a
Samuel Ketels <ketels.samuel@gmail.com>
2020-07-06 11:54:21 +0200

    projects main

--- Portfolio/Portfolio/Pages/Projects/ProjectsMain.cshtml.cs
+++ Portfolio/Portfolio/Pages/Projects/ProjectsMain.cshtml.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Core;
+using Portfolio.Data;
 
 namespace Portfolio
 {
     public class ProjectsMainModel : PageModel
     {
+        private readonly IProjectData _projectData;
+
+        public IEnumerable<PersonalProject> PersonalProjects { get; set; }
+        public ProjectsMainModel(IProjectData projectData)
+        {
+            _projectData = projectData;
+        }
         public void OnGet()
         {
 
