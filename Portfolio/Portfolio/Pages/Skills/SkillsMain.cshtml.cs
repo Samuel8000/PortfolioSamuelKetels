8773f82cbc4f26569cf388b5fd91519c9caadb87
Samuel Ketels <ketels.samuel@gmail.com>
2020-07-16 16:35:38 +0200

    removed modal

--- Portfolio/Portfolio/Pages/Skills/SkillsMain.cshtml.cs
+++ Portfolio/Portfolio/Pages/Skills/SkillsMain.cshtml.cs
@@ -12,29 +12,17 @@ namespace Portfolio
     public class SkillsMainModel : PageModel
     {
         private readonly ISkillData _skillData;
-        private readonly ICertificateData _certificateData;
 
         public IEnumerable<Skill> Skills { get; set; }
 
-        [BindProperty]
-        public IEnumerable<Certificate> Certificates { get; set; }
-
-        public SkillsMainModel(ISkillData skillData,ICertificateData certificateData)
+        public SkillsMainModel(ISkillData skillData)
         {
             _skillData = skillData;
-            _certificateData = certificateData;
         }
         public void OnGet()
         {
             Skills = _skillData.GetAllSkills();
-
         }
 
-        public IActionResult OnGetCertificates(int skillId)
-        {
-            Certificates = _certificateData.GetCertificatesBySkill(skillId);
-            return RedirectToPage("/Skills/SkillDetail", new { skillId = Certificates.Where(c => c.SkillId == skillId )});
-
-        }
     }
 }
